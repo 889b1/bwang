42a2a1c6eb31694afecc3237f5ec184640723025
889b1 <889b@anonymousspeech.com>
2017-09-08 01:50:39 +0100

    Two-way binding with v-models on input boxes

---
+++ client/src/components/Register.vue
@@ -0,0 +1,48 @@
+<template>
+   <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="user@nigerian.system ..." />
+
+      <br>
+
+      <input
+        type="password"
+        name="password"
+        v-model="password"
+        placeholder="**********" />
+      <br>
+
+      <buttonRegister</button>
+ </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: 'user@nigerian.system',
+      password: '*******'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed value', value)
+    }
+  },
+  mounted () {
+    setTimeout(function () {
+      this.email = 'hello world'
+    }, 20000)
+  }
+}
+
+</script>
+\
+<style scoped>
+
+</style>
